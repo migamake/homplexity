b376fb94b5bcfb6bff6ca90aa16016f9a5116aca
Michal J. Gajda <migamake@migamake.com>
2018-11-19 12:50:31 +0100

    Expand .gitignore

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,5 @@
 cabal configure --enable-documentation
 cabal sdist
-cabal upload --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD"              dist/homplexity-*[0-9].tar.gz
+cabal upload --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD"    dist/homplexity-*[0-9].tar.gz
 cabal haddock --for-hackage
-cabal upload --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD" --publish -d dist/homplexity-*-docs.tar.gz
+cabal upload --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD" -d dist/homplexity-*-docs.tar.gz
