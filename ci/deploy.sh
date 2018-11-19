9355f8752930bf1e2a59f95a7b79b414a1540dde
Michal J. Gajda <migamake@migamake.com>
2018-11-19 03:12:55 +0100

    New deployment script

---
+++ ci/deploy.sh
@@ -0,0 +1,4 @@
+cabal sdist
+cabal upload --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD"    dist/homplexity-*[0-9].tar.gz
+cabal haddock --for-hackage
+cabal upload --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD" -d dist/homplexity-*-docs.tar.gz
