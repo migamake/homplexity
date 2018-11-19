0e854eaf802e16613863dfa0fbadabf08bcdc8a8
Michal J. Gajda <migamake@migamake.com>
2018-11-19 10:55:44 +0100

    Fixing .travis.yml

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,4 @@
+cabal configure --enable-documentation
 cabal sdist
 cabal upload --username="$HACKAGE_USER" --password="$HACKAGE_PASSWORD"              dist/homplexity-*[0-9].tar.gz
 cabal haddock --for-hackage
