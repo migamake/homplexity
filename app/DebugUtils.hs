312732fa6b80e0c3e60666e91224b4b8339cadff
Dmitry Krylov <dima@krylov.ws>
2019-09-06 18:57:56 +1000

    Cleanup code

--- app/DebugUtils.hs
+++ app/DebugUtils.hs
@@ -1,38 +1,11 @@
-{-# LANGUAGE DeriveDataTypeable    #-}
-{-# LANGUAGE LambdaCase            #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE RecordWildCards       #-}
-{-# LANGUAGE StandaloneDeriving    #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE UndecidableInstances  #-}
-{-# LANGUAGE ViewPatterns          #-}
--- | Main module parsing inputs, and running analysis.
+-- | Just testing some homplexity stuff...
+--
 module Main (main) where
 
 
-import Data.Functor
-import Data.List
-import Data.Monoid
-
-import Language.Haskell.Exts.SrcLoc
-import Language.Haskell.Exts.Syntax
-import Language.Haskell.Homplexity.Assessment
-import Language.Haskell.Homplexity.CodeFragment
-import Language.Haskell.Homplexity.Message
 import Language.Haskell.Homplexity.Parse
-import System.Directory
-import System.Exit
-import System.FilePath
-import System.IO
-import           Data.Map.Strict    (Map)
-import qualified Data.Map.Strict as Map
-
 import Language.Haskell.Exts
-
 import Text.Pretty.Simple
-import Data.Maybe
 
 
 pp :: (Show a) => a -> IO ()
@@ -41,17 +14,7 @@ pp = pPrintOpt NoCheckColorTty defaultOutputOptionsDarkBg
 
 main :: IO ()
 main = do
-    let testFn = "test-data/test0001.hs"
-        {-
-    file <- readFile testFn
-    let exts = readExtensions file
-    let exts' = maybe [] snd exts
-    pp exts
-    pp exts'
-    putStrLn "*************"
-    pp (collapseSameExtensions exts')
-    pp (collapseSameExtensions [ DisableExtension ScopedTypeVariables, EnableExtension ScopedTypeVariables, EnableExtension DoRec ])
-    -}
+    let testFn = "tests/test-data/test0001.hs"
     src <- parseSource [EnableExtension ScopedTypeVariables] testFn
     pp src
 
