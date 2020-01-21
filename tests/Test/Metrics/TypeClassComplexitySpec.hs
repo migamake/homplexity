51b79ec046916557625ab8cc7cd72c7951d80546
anabra <peter.d.podlovics@gmail.com>
2020-01-21 12:47:48 +0100

    Extracted the common functionality from TC complexity tests

--- tests/Test/Metrics/TypeClassComplexitySpec.hs
+++ tests/Test/Metrics/TypeClassComplexitySpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 module Test.Metrics.TypeClassComplexitySpec where
 
 
@@ -6,25 +7,16 @@ import Test.Utilities
 
 import Language.Haskell.Exts.Extension
 import Language.Haskell.Homplexity.TypeClassComplexity
-import Language.Haskell.Homplexity.CodeFragment (TypeClass, occurs)
-import Language.Haskell.Homplexity.Metric (measure)
-import Language.Haskell.Homplexity.Parse (parseSource)
+import Language.Haskell.Homplexity.CodeFragment (TypeClass)
 
 spec :: Spec
 spec = describe "tests for type class complexity" $ do
   it "correctly counts methods and values" $ flip shouldReturn (5 :: NonTypeDeclCount) $ do
-    result <- parseSource [] (testFile "TypeClassComplexityTest.hs")
-    p <- case result of
-      Left  logs      -> error $ show logs
-      Right (prog, _) -> pure prog
-    let tyClassDecls = occurs p :: [TypeClass]
-    pure $ sum $ map measure tyClassDecls
+    measureSumOnFile @NonTypeDeclCount @TypeClass
+      [] (testFile "TypeClassComplexityTest.hs")
 
   it "correctly counts associated types" $ flip shouldReturn (5 :: AssocTypeCount) $ do
-    result <- parseSource [EnableExtension TypeFamilies] (testFile "TypeClassComplexityTest.hs")
-    p <- case result of
-      Left  logs      -> error $ show logs
-      Right (prog, _) -> pure prog
-    let tyClassDecls = occurs p :: [TypeClass]
-    pure $ sum $ map measure tyClassDecls
+    measureSumOnFile @AssocTypeCount @TypeClass
+      [EnableExtension TypeFamilies] (testFile "TypeClassComplexityTest.hs")
+
 
