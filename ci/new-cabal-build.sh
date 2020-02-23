5d9ddeddc6f25f41edffee85140cb0e8615634c5
Michal J. Gajda <mjgajda@migamake.com>
2020-02-23 16:04:38 +0100

    Remove extraneous --allow-newer.

--- ci/new-cabal-build.sh
+++ ci/new-cabal-build.sh
@@ -16,8 +16,8 @@ cabal test --allow-newer
 message "Prepare artifacts"
 mkdir -p bin sdist
 cabal install --bindir=bin/ --allow-newer
-cabal sdist   --builddir=sdist/ --allow-newer
-cabal haddock --builddir hackage-docs --haddock-for-hackage --allow-newer
+cabal sdist   --builddir=sdist/
+cabal haddock --builddir hackage-docs --haddock-for-hackage
 
 message "Test on own source"
 cabal exec -- homplexity-cli lib/ # path is dist/build/homplexity-cli
