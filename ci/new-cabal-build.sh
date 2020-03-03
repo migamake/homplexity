a327f490b4e42ef606943944e7bea59322d0bc00
Michal J. Gajda <mjgajda@migamake.com>
2020-03-03 12:41:34 +0100

    Overwrite artifacts in CI build.

--- ci/new-cabal-build.sh
+++ ci/new-cabal-build.sh
@@ -15,7 +15,7 @@ cabal test --allow-newer
 
 message "Prepare artifacts"
 mkdir -p bin sdist
-cabal install --bindir=bin/ --allow-newer
+cabal install --bindir=bin/ --allow-newer --overwrite-policy=always
 cabal sdist   --builddir=sdist/
 cabal haddock --builddir hackage-docs --haddock-for-hackage
 
